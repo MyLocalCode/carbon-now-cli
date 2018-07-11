37cdffd24ae33f78c0e2c52b312817eb64aa16f3
MyLocalCode <usert28@yahoo.com>
2018-07-11 12:59:06 +0530

    Update headless-visit.test.js

--- test/headless-visit.test.js
+++ test/headless-visit.test.js
@@ -1,6 +1,9 @@
 // Packages
 import test from 'ava';
 
+// Source
+import headlessVisit from '../src/headless-visit';
+
 test('Fails due to wrong URL/timeout/event', async t => {
 	try {
 		await headlessVisit('foobar');
